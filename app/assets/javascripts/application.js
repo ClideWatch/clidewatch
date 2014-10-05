ab12e2647d8de7e9c78effd0c3c694a379e398d0
bill-auger <mr.j.spam.me@gmail.com>
2014-10-05 12:55:50 +0000

    style the homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,19 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+var CURSOR_BLINK_IVL = 750 ;
+
+
+var BlinkToggle = true ;
+
+
+window.setInterval(function()
+{
+  var promptCursorSpan = document.getElementById('promptCursorSpan') ;
+
+  if (promptCursorSpan)
+    promptCursorSpan.style.visibility = (BlinkToggle = !BlinkToggle)?
+                                        'visible' : 'hidden'  ;
+} , CURSOR_BLINK_IVL) ;
